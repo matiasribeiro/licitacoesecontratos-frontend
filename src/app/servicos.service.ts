4f62e296980c893e835f6cec3e042e2f29efa20c
Matias <mathias.ribeiro@gmail.com>
2021-05-13 20:01:37 -0300

    refatoração de código e mudança do layout para consulta de contratos

--- src/app/servicos.service.ts
+++ src/app/servicos.service.ts
@@ -10,8 +10,9 @@ import { Contratos } from './modelos/contratos';
 
 
 
-/* const API_ENDPOINT = 'https://apilicitacoesecontratos.herokuapp.com'; */
- const API_ENDPOINT = 'http://localhost:8080';
+const API_ENDPOINT = 'https://apilicitacoesecontratos.herokuapp.com';
+ /*const API_ENDPOINT = 'http://localhost:8080';*/
+
 
 
 @Injectable({
