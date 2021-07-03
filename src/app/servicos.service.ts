f5e04f29835e267a0ff8f64017b5f6b610ecade7
Matias <mathias.ribeiro@gmail.com>
2021-07-03 16:32:10 -0300

    bug consulta de fornecedor de contratos

--- src/app/servicos.service.ts
+++ src/app/servicos.service.ts
@@ -36,6 +36,8 @@ export class ServicosService {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Origin': '*'
     })
   }
 
